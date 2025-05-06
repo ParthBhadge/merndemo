bd33ebc9b4b6edb6ea2038851b2b6d25104aee94
Parth Raju Bhadge <99082524+ParthBhadge@users.noreply.github.com>
2025-05-07 02:08:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+app.use('/', (req, res) => {
+  res.send('API is running...');
+});
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
